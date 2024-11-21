ac869fdfc0f0fa784d20f732fe1b2c933d69c057
NihadRasulzada <nihadrs2005@gmail.com>
2024-11-21 17:44:45 +0400

    firstpushjenkins

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,7 @@ namespace MultiShop.IdentityServer
                 .AddGoogle(options =>
                 {
                     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    
+
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
                     // set the redirect URI to https://localhost:5001/signin-google
